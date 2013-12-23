4cd8f96fd659e7c33f4dd34b6ba48fa5cdd39164
Yixuan Qiu <yixuan.qiu@cos.name>
2013-12-23 17:43:31 -0500

    initial commit

---
+++ header.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+  <link rel="shortcut icon" href="http://cos.name/wp-content/themes/COS-kermesinus/images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="http://cos.name/wp-content/themes/COS-kermesinus/images/favicon.ico" type="image/ico">
+
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+  
+  <?php /* hard-coded. It's bad, but I need to use it. --Yixuan */ ?>
+  <style type="text/css">
+    #cos-logo {
+      width: 100%;
+      text-align: center;
+    }
+    #cos-logo img {
+      width: 90%;
+      max-width: 521px;
+      max-height: 120px;
+    }
+	</style>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+      
+      <?php /* hard-coded. It's bad, but I need to use it. --Yixuan */ ?>
+      <div id="cos-logo">
+          <a href="http://cos.name/">
+            <img src="http://cos.name/wp-content/themes/COS-kermesinus/images/headers/cos-logo.png" />
+          </a>
+      </div>
+
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+					<?php get_search_form(); ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
