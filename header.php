515cf75b2f775c96c7936020aa7aae6a78e31718
Yixuan Qiu <yixuan.qiu@cos.name>
2014-11-08 20:41:40 -0500

    tidy code

--- header.php
+++ header.php
@@ -19,53 +19,50 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico" type="image/ico">
 
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
-	<![endif]-->
-	<?php wp_head(); ?>
-  
-  <?php /* hard-coded. It's bad, but I need to use it. --Yixuan */ ?>
-  <style type="text/css">
-    #cos-logo {
-      width: 100%;
-      text-align: center;
-    }
-    #cos-logo img {
-      width: 90%;
-      max-width: 521px;
-      max-height: 120px;
-    }
-	</style>
+  <!--[if lt IE 9]>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+  <![endif]-->
+  <?php wp_head(); ?>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-1988641-10', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="page" class="hfeed site">
-		<header id="masthead" class="site-header" role="banner">
+  <div id="page" class="hfeed site">
+    <header id="masthead" class="site-header" role="banner">
       
       <?php /* hard-coded. It's bad, but I need to use it. --Yixuan */ ?>
       <div id="cos-logo">
-          <a href="http://cos.name/">
-            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/headers/cos-logo.png" />
-          </a>
+        <a href="http://cos.name/">
+          <img src="<?php echo get_stylesheet_directory_uri() ?>/images/headers/cos-logo.png" />
+        </a>
       </div>
 
-			<div id="navbar" class="navbar">
-				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
-				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
-		</header><!-- #masthead -->
+      <div id="navbar" class="navbar">
+        <nav id="site-navigation" class="navigation main-navigation" role="navigation">
+          <h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+          <a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+          <?php get_search_form(); ?>
+        </nav><!-- #site-navigation -->
+      </div><!-- #navbar -->
+    </header><!-- #masthead -->
 
-		<div id="main" class="site-main">
+    <div id="main" class="site-main">
